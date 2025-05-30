8b868c106c2a7b185a8f697bd6efd273977f4648
Diandreas <127276266+Diandreas@users.noreply.github.com>
2025-05-30 21:53:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
+                'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/bill-create.js'
             ],
             refresh: true,
         }),
